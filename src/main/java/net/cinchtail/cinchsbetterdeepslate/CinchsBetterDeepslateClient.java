149b5a44b0e152af02cca70877574ceba22a0c9d
cinchtail <cinchtail@gmail.com>
2024-05-25 00:02:12 -0400

    Initial commit

---
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslateClient.java
@@ -0,0 +1,10 @@
+package net.cinchtail.cinchsbetterdeepslate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CinchsBetterDeepslateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
